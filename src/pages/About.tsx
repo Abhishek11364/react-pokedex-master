f152f2868118951c3c091dc1aaff9968866111a9
abhi maurya <98451783+Abhishek11364@users.noreply.github.com>
2024-01-23 10:01:18 +0000

    Update1

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import Wrapper from "../sections/Wrapper";
-import avatarImage from "../assets/kishan.jpeg";
-import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
+
+import {  FaGithub, FaLinkedin } from "react-icons/fa";
 
 function About() {
   return (
     <div className="profile">
-      <img src={avatarImage} alt="" className="profile-image" />
-      <h1 className="profile-text">Hi I am kishan Sheth</h1>
-      <h2 className="profile-text">The creator of this awesome pokedex</h2>
-      <h4 className="profile-text">
-        This project is created for youtube tutorial for my channel Kishan Sheth
-      </h4>
+      <img src="https://i.postimg.cc/tR6NxdrQ/profile-pic-3.png" alt="" className="profile-image" />
+      <h1 className="profile-text">Hi I am Abhishek Kushwaha</h1>
+      <h2 className="profile-text">The creator of this awesome pokeWeb</h2>
+     
       <div className="profile-links">
-        <a href="https://github.com/koolkishan">
+        <a href="https://github.com/Abhishek11364">
           <FaGithub />
         </a>
-        <a href="https://www.youtube.com/c/KishanSheth21">
-          <FaYoutube />
-        </a>
-        <a href="https://www.linkedin.com/in/koolkishan/">
+       
+        <a href="linkedin.com/in/abhishek-kushwaha1508
+">
           <FaLinkedin />
         </a>
       </div>
