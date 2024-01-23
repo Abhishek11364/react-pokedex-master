438d823e6a27243cf835ed99dfc0e246f3e0e44e
abhi maurya <98451783+Abhishek11364@users.noreply.github.com>
2024-01-23 10:43:56 +0000

    Update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,20 +6,17 @@ import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
 function About() {
   return (
     <div className="profile">
-      <img src={avatarImage} alt="" className="profile-image" />
-      <h1 className="profile-text">Hi I am kishan Sheth</h1>
-      <h2 className="profile-text">The creator of this awesome pokedex</h2>
-      <h4 className="profile-text">
-        This project is created for youtube tutorial for my channel Kishan Sheth
-      </h4>
+      <img src="https://i.postimg.cc/tCW5NpDg/profile-pic-1.png" alt="" className="profile-image" />
+      <h1 className="profile-text">Hi I am Abhishek Kushwaha</h1>
+      <h2 className="profile-text">The creator of this  pokedex</h2>
+     
       <div className="profile-links">
-        <a href="https://github.com/koolkishan">
+        <a href="https://github.com/Abhishek11364">
           <FaGithub />
         </a>
-        <a href="https://www.youtube.com/c/KishanSheth21">
-          <FaYoutube />
-        </a>
-        <a href="https://www.linkedin.com/in/koolkishan/">
+      
+        <a href="linkedin.com/in/abhishek-kushwaha1508
+">
           <FaLinkedin />
         </a>
       </div>
